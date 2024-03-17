9c240b5fa8725d5843890355526d75be3475c68f
Timofey <storozhilovtimofey@gmail.com>
2024-03-17 21:16:19 +0300

    Перенес все в общий файл с кодом, потом переведу на ООП

--- GeneralRAMData.cpp
+++ GeneralRAMData.cpp
@@ -59,6 +59,37 @@ std::string GetFirstWord(const std::string& line)
     return word;
 }
 
+void ChangetoValidOutput(std::string& line)
+{
+    std::string word;
+    float number;
+    std::istringstream iss(line);
+    iss >> word >> number;
+    number /= 1048576;
+    if (word == "MemTotal:") { std::cout << "Total Memory: " << number << " Gb" << std::endl; }
+    else if (word == "MemFree:") { std::cout << "Free Memory: " << number << " Gb" << std::endl; }
+    else { std::cout << word << number * 1024 << " Mb" << std::endl; }
+}
+
+void GeneralRamParametres(const std::string& path, std::vector<std::string>& Needs)
+{   
+    std::cout << "General Parametres: " << std::endl;
+    std::ifstream file(path);
+    std::string line;
+    while (std::getline(file, line))
+    {   
+        for (const std::string& start : Needs)
+        {
+            if (GetFirstWord(line).compare(0, start.length(), start) == 0)
+            {
+                ChangetoValidOutput(line);
+                break;
+            }
+        }
+    }
+    file.close();
+}
+
 void deleteFile(const std::string& filename)
 {
     std::string command = "rm -f " + filename; // создаем строку команды для удаления файла
@@ -129,8 +160,19 @@ int main()
     "Manufacturer: Not Specified",
     "Serial Number: Not Specified"
     };
+    const std::string path = "/proc/meminfo";
+    std::vector<std::string> Needs = 
+    {
+        "MemTotal:",
+        "MemFree:",
+        "Buffers:",
+        "Cached:"
+    };
     printMatchingLines(Allowed, Exception, filePath);
     deleteFile("memory_info.txt");
+    std::cout << std::endl;
+    std::cout << std::endl;
+    GeneralRamParametres(path, Needs);
 
     return 0;
 }
